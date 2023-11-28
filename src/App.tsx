52dfb200ca09aeba98a883d6a34157c44ba8d1c5
Shakhdv <shakhidov063@mail.ru>
2023-11-29 00:13:32 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Sidebar from './components/Sidebar/Sidebar'
+import SignIn from './components/Sign/SignIn'
+import SignUp from './components/Sign/SignUp'
 
 function App() {
   return (
