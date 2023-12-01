577624f1e582b78dcf3a925d6b66da6a7b9edd4a
Shakhdv <shakhidov063@mail.ru>
2023-12-02 01:48:15 +0300

    startScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import Sidebar from './components/Sidebar/Sidebar'
-import SignIn from './components/Sign/SignIn'
-import SignUp from './components/Sign/SignUp'
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
+import SignIn from "./components/Sign/SignIn";
+import SignUp from "./components/Sign/SignUp";
 import StartScreen from "./components/startScreen/StartScreen";
 
 function App() {
   return (
     <>
-    <div>
-      <Header/>
-      <StartScreen/>
-      <Footer/>
-    </div>
+      <div>
+        <Header />
+        <StartScreen />
+        <Routes>
+          <Route path="/" element={<StartScreen />} />
+          <Route path="/login" element={<SignIn />} />
+          <Route path="/sign" element={<SignUp />} />
+        </Routes>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/features/categoriesSlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+  categories: [],
+  loading: false,
+  error: null,
+};
+
+export const getCategories = createAsyncThunk("get/category", async (_, thunkAPI) => {
+  try {
+    const res = await fetch("http://localhost:4000/category");
+    const data = await res.json();
+
+    if (data.error) {
+      return thunkAPI.rejectWithValue(data.error);
+    }
+
+    return data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
+
+export const categoriesSlice = createSlice({
+  name: "categories",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCategories.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getCategories.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.categories = action.payload;
+      })
+  },
+});
+
+export default categoriesSlice.reducer;
