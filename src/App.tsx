cbc3f6589d154962a7881fcb7061c08e05b25832
MShakhabov <ya.shahabov1@gmail.com>
2023-12-06 17:21:16 +0300

    много чего сделано

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,38 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Sidebar from "./components/Sidebar/Sidebar";
 import SignIn from "./components/Sign/SignIn";
 import SignUp from "./components/Sign/SignUp";
 import StartScreen from "./components/startScreen/StartScreen";
 import Programs from "./components/Programs/Programs";
 import OneProgram from "./components/Programs/OneProgram";
-
 import { useSelector } from "react-redux";
 import MyPrograms from "./components/MyPrograms/MyPrograms";
 import Lessons from "./components/Lessons/Lessons";
 import OneLesson from "./components/Lessons/OneLesson";
+import KartsTraining from "./components/pageInfo/OrderTraining/KartsTraining";
+import WhyUsKarts from "./components/pageInfo/WhyUs/WhyUsKarts";
+import OurProgramms from "./components/pageInfo/OurProgramms/OurProgramms";
+import Consultation from "./components/pageInfo/Consultation/Consultation";
 
 function App() {
   const token = useSelector((state) => state.application.token);
-// const user = useSelector((state) => state.application.user)
 
   return (
     <>
       <div>
         <Header />
-        
-        
         <Routes>
-
           {token ? (
             <>
               <Route path="/login" element={<Navigate to="/" />} />
               <Route path="/signup" element={<Navigate to="/" />} />
               <Route path="/mycourse" element={<MyPrograms />} />
               <Route path="/mycourse/:programId" element={<Lessons />} />
-              <Route path="/lessons/:programId/:lessonId" element={<OneLesson/>} />
+              <Route
+                path="/lessons/:programId/:lessonId"
+                element={<OneLesson />}
+              />
             </>
           ) : (
             <>
@@ -48,8 +49,6 @@ function App() {
           <Route path="/kart" element={<WhyUsKarts />} />
           <Route path="/pro" element={<OurProgramms />} />
           <Route path="/consult" element={<Consultation />} />
-
-          
           <Route path="/program/:program" element={<OneProgram />} />
         </Routes>
         <Footer />
